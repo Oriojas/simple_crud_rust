6c3b65f1a06b6b06f296fdeb9db25692e0ca80ae
oscar50 <oscar@whaleandjaguar.co>
2023-05-23 20:13:12 -0500

    feat: create front input data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate diesel;
 extern crate dotenv;
+extern crate gtk;
+
 
 use diesel::prelude::*;
 use dotenv::dotenv;
@@ -7,6 +9,8 @@ use models::Post;
 use service::PostService;
 use std::env;
 use std::ops::Add;
+use gtk::{prelude::*, Orientation};
+use gtk::{Button, Entry, Label, Window, WindowType};
 
 mod schema;
 mod models;
@@ -52,6 +56,74 @@ fn update_post(conn: &mut MysqlConnection, post: &Post, new_name: &str) -> Post 
 }
 
 fn main() {
+    gtk::init().expect("No se puedo iniciar la ventana GTK");
+
+    let window: Window = Window::new(WindowType::Toplevel);
+    window.set_title("Polkadot curso Rust");
+    window.set_default_size(320, 200);
+
+    let container = gtk::Box::new(gtk::Orientation::Vertical, 8);
+    window.add(&container);
+
+    let name_val = Label::new(Some("Introduzca su nombre"));
+    container.add(&name_val);
+
+    let entry_name = Entry::new();
+    container.add(&entry_name);
+
+    let label_name = Label::new(None);
+    container.add(&label_name);
+
+
+    let last_name_val = Label::new(Some("Introduzca su apellido"));
+    container.add(&last_name_val);
+
+    let entry_last_name = Entry::new();
+    container.add(&entry_last_name);
+
+    let label_last_name = Label::new(None);
+    container.add(&label_last_name);
+
+
+    let email_val = Label::new(Some("Introduzca su email"));
+    container.add(&email_val);
+
+    let entry_email = Entry::new();
+    container.add(&entry_email);
+
+    let label_email = Label::new(None);
+    container.add(&label_email);
+
+    let button = Button::with_label("Guardar");
+    button.connect_clicked(move |_| {
+        let text_name = entry_name.get_text().to_string();
+        label_name.set_text(&text_name);
+        let text_last_name = entry_last_name.get_text().to_string();
+        label_last_name.set_text(&text_last_name);
+        let text_email = entry_email.get_text().to_string();
+        label_email.set_text(&text_email);
+
+        let mut post_service = PostService::new();
+
+        let new_post = post_service.create_post(&text_name, &text_last_name, &text_email);
+    
+        let data_from_repository = post_service.get_posts();
+        for post in data_from_repository {
+            println!("{:?}", post);
+        }
+    
+    });
+    container.add(&button);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    window.show_all();
+
+    gtk::main();
+
 
     //////////////////////////////////////////////////
     ////////  Trabajar unicamente con Diesel   ///////
@@ -76,18 +148,6 @@ fn main() {
     ///////////////////////////////////////////////////////
     ////////  Trabajar Patrones Diseño con Diesel   ///////
     ///////////////////////////////////////////////////////
-    let new_name = "Oscar";
-    let new_last_name = "Riojas";
-    let new_email = "oscar@mail.mail";
-
-    let mut post_service = PostService::new();
-
-    let new_post = post_service.create_post(new_name, new_last_name, new_email);
-
-    let data_from_repository = post_service.get_posts();
-    for post in data_from_repository {
-        println!("{:?}", post);
-    }
 
 
 }
